87af64d1d6b44303ebeea95170d384f08a989ada
LucasSaller <sallerlucas@gmail.com>
2023-06-17 18:21:17 -0300

    Add fonts icons, and create TopTitle header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { ReactComponent as Add } from "./assets/icons/add.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="App-header">
+      <div className="container">
+        <div className="page-wrapper">
+          <div className="top-title">
+            <h2>Task List</h2>
+            <button className="button">
+              <span style={{ margin: "0 15px 0 0" }}>
+                <Add />
+              </span>
+              Add Task
+            </button>
+          </div>
+        </div>
+      </div>
+    </header>
   );
 }
 
