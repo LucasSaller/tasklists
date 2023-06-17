f1e63ea4e40ae4228da90249314d0d7eff69cd9a
LucasSaller <sallerlucas@gmail.com>
2023-06-17 20:10:03 -0300

    Add state to control the visibility of the modal

--- src/App.js
+++ src/App.js
@@ -2,25 +2,28 @@ import "./App.css";
 import { ReactComponent as Add } from "./assets/icons/add.svg";
 import { taskList } from "./data/data";
 import TaskCard from "./components/TaskCard/TaskCard";
-import AddEditTaskForm from "./components/TaskCard/AddEditTaskForm/AddEditTaskForm";
+import AddEditTaskForm from "./components/AddEditTaskForm/AddEditTaskForm";
+import { useState } from "react";
 function App() {
+  const [openModal, setOpenModal] = useState(false);
+
   return (
     <header className="App-header">
       <div className="container">
         <div className="page-wrapper">
           <div className="top-title">
             <h2>Task List</h2>
-            <button className="button">
+            <button className="button" onClick={() => setOpenModal(true)}>
               <span style={{ margin: "0 15px 0 0" }}>
                 <Add />
               </span>
               Add Task
             </button>
           </div>
-          <AddEditTaskForm />
+          {openModal && <AddEditTaskForm setOpenModal={setOpenModal} />}
           <div className="tasks-container">
             {taskList.map((task) => (
-              <TaskCard task={task} />
+              <TaskCard task={task} key={task.id} />
             ))}
           </div>
         </div>
